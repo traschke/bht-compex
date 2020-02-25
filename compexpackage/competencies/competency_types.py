6c1d6bdc22aa91de61f16d272ff1909018f9bc42
Timo <timo.raschke@gmail.com>
2020-02-25 16:57:01 +0100

    Done stuff

--- compexpackage/competencies/competency_types.py
+++ compexpackage/competencies/competency_types.py
@@ -12,13 +12,25 @@ class WordChunk:
         else:
             self.words: List[Word] = words
 
-class Sentence:
-    def __init__(self, words: List[Word], competencies: List[Competency] = None):
-        self.words: List[Word] = []
-        if competencies is None:
-            self.competencies: List[Competency] = []
+class ObjectContext:
+    def __init__(self, word_chunk: WordChunk):
+        self.word_chunk: WordChunk = word_chunk
+
+    def __str__(self) -> str:
+        return " ".join(x.word for x in self.word_chunk.words)
+class CompetencyObject:
+    def __init__(self, word_chunk: WordChunk, contexts: List[ObjectContext] = None):
+        self.word_chunk: WordChunk = word_chunk
+        if contexts is None:
+            self.contexts: List[ObjectContext] = []
         else:
-            self.competencies: List[Competency] = competencies
+            self.contexts: List[ObjectContext] = contexts
+
+    def __str__(self) -> str:
+        return " ".join(x.word for x in self.word_chunk.words)
+
+    def __repr__(self):
+        return self.__str__()
 
 class Competency:
     def __init__(self, word: Word, objects: List[CompetencyObject] = None):
@@ -28,14 +40,16 @@ class Competency:
         else:
             self.objects: List[CompetencyObject] = objects
 
-class CompetencyObject:
-    def __init__(self, word_chunk: WordChunk, contexts: List[ObjectContext] = None):
-        self.word_chunk: WordChunk = word_chunk
-        if contexts is None:
-            self.contexts: List[ObjectContext] = []
-        else:
-            self.contexts: List[ObjectContext] = contexts
+    def __str__(self) -> str:
+        return "{}: {}".format(self.word.word, str(self.objects))
 
-class ObjectContext:
-    def __init__(self, word_chunk: WordChunk):
-        self.word_chunk: WordChunk = word_chunk
+class Sentence:
+    def __init__(self, words: List[Word], competencies: List[Competency] = None):
+        self.words: List[Word] = []
+        if competencies is None:
+            self.competencies: List[Competency] = []
+        else:
+            self.competencies: List[Competency] = competencies
+
+    def __str__(self) -> str:
+        return " ".join(x.word for x in self.words)
