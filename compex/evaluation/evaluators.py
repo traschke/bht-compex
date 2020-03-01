b3ac730c2cae6e43574b0d252873d3538b178b70
Timo <timo.raschke@gmail.com>
2020-03-01 18:42:21 +0100

    Refactore code

--- compex/evaluation/evaluators.py
+++ compex/evaluation/evaluators.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+from enum import Enum
 
 from compex.competencies.competency_types import Competency
 from compex.annotators import SemgrexAnnotator
@@ -24,6 +25,10 @@ class EvaluationSet:
 
         return merged_data
 
+class CalculationType(Enum):
+    POSITIVE = 0,
+    NEGATIVE = 1
+
 class FMeasureEvaluator:
     """Evaluator precision, recall and f1-score"""
 
@@ -34,82 +39,12 @@ class FMeasureEvaluator:
         Use the appropriate parameters to also consider objects and contexts of competencies.
         """
 
-        # Fetch true positives, false positives and false negatives competencies, objects and contexts
         # TODO Take objects and contexts into account!
-        true_positives = 0
-        false_positives = 0
-        true_negatives = 0
-        false_negatives = 0
-
         # Calculate true positives and false positives
-        for sentence, data in evaluation_set.merged_data.items():
-            if len(data) == 2:
-                for annotated_data_competency in data["annotated_data"]:
-                    competency_is_found: bool = False
-                    for test_data_compentency in data["test_data"]:
-                        if test_data_compentency.word == annotated_data_competency.word:
-                            competency_is_found = True
-                            true_positives += 1
-                            if consider_objects:
-                                # Check the competencies objects
-                                for annotated_object in annotated_data_competency.objects:
-                                    is_whole_object_found: bool = False
-                                    for test_data_object in test_data_compentency.objects:
-                                        if test_data_object == annotated_object:
-                                            is_whole_object_found = True
-                                            # Add the count of the objects words to
-                                            true_positives += len(annotated_object.word_chunk.words)
-                                            break
-                                    # Check each word of objects if the whole object is not correct
-                                    if not is_whole_object_found:
-                                        for annotated_object_word in annotated_object.word_chunk.words:
-                                            for test_data_object in test_data_compentency.objects:
-                                                is_object_word_found: bool = False
-                                                for test_data_object_word in test_data_object.word_chunk.words:
-                                                    if annotated_object_word == test_data_object_word:
-                                                        is_object_word_found = True
-                                                        true_positives += 1
-                                                        break
-                                                if not is_object_word_found:
-                                                    false_positives += 1
-                            break
-                    if not competency_is_found:
-                        false_positives += 1
+        true_positives, false_positives = self.__count_positive_negative(evaluation_set, CalculationType.POSITIVE, consider_objects, consider_contexts)
 
-        # Calculate false negatives and true negatives
-        for sentence, data in evaluation_set.merged_data.items():
-            if len(data) == 2:
-                for test_data_compentency in data["test_data"]:
-                    competency_is_found: bool = False
-                    for annotated_data_competency in data["annotated_data"]:
-                        if test_data_compentency == annotated_data_competency:
-                            competency_is_found = True
-                            true_negatives += 1
-                            if consider_objects:
-                                # Check the competencies objects
-                                for test_data_object in test_data_compentency.objects:
-                                    is_whole_object_found: bool = False
-                                    for annotated_object in annotated_data_competency.objects:
-                                        if test_data_object == annotated_object:
-                                            is_whole_object_found = True
-                                            # Add the count of the objects words to
-                                            true_negatives += len(annotated_object.word_chunk.words)
-                                            break
-                                    # Check each word of objects if the whole object is not correct
-                                    if not is_whole_object_found:
-                                        for test_data_object_word in test_data_object.word_chunk.words:
-                                            for annotated_data_object in annotated_data_competency.objects:
-                                                is_object_word_found: bool = False
-                                                for annotated_object_word in annotated_data_object.word_chunk.words:
-                                                    if annotated_object_word == test_data_object_word:
-                                                        is_object_word_found = True
-                                                        true_negatives += 1
-                                                        break
-                                                if not is_object_word_found:
-                                                    false_negatives += 1
-                            break
-                    if not competency_is_found:
-                        false_negatives += 1
+        # Calculate true negatives and false negatives
+        true_negatives, false_negatives = self.__count_positive_negative(evaluation_set, CalculationType.NEGATIVE, consider_objects, consider_contexts)
 
         precision = self.__calculate_precision(true_positives, false_positives)
         recall = self.__calculate_recall(true_positives, false_negatives)
@@ -124,6 +59,58 @@ class FMeasureEvaluator:
             "true_negatives": true_negatives,
             "false_negatives": false_negatives
             }
+
+    def __count_positive_negative(self, evaluation_set: EvaluationSet, mode: CalculationType = CalculationType.POSITIVE, consider_objects: bool = False, consider_contexts: bool = False):
+        """
+        Counts either positive or negative based objects in evaluationset.
+        Default is positive.
+        """
+        trues = 0
+        falses = 0
+
+        for _, data in evaluation_set.merged_data.items():
+            if len(data) == 2:
+                # Switch dicts if needed according to positive or negative mode
+                if mode == CalculationType.POSITIVE:
+                    dict1 = data["annotated_data"]
+                    dict2 = data["test_data"]
+                elif mode == CalculationType.NEGATIVE:
+                    dict1 = data["test_data"]
+                    dict2 = data["annotated_data"]
+
+                for dict1_competency in dict1:
+                    competency_is_found: bool = False
+                    for dict2_compentency in dict2:
+                        if dict2_compentency.word == dict1_competency.word:
+                            competency_is_found = True
+                            trues += 1
+                            if consider_objects:
+                                # Check the competencies objects
+                                for dict1_object in dict1_competency.objects:
+                                    is_whole_object_found: bool = False
+                                    for dict2_object in dict2_compentency.objects:
+                                        if dict2_object == dict1_object:
+                                            is_whole_object_found = True
+                                            # Add the count of the objects words to
+                                            trues += len(dict1_object.word_chunk.words)
+                                            break
+                                    # Check each word of objects if the whole object is not correct
+                                    if not is_whole_object_found:
+                                        for dict1_object_word in dict1_object.word_chunk.words:
+                                            for dict2_object in dict2_compentency.objects:
+                                                is_object_word_found: bool = False
+                                                for dict2_object_word in dict2_object.word_chunk.words:
+                                                    if dict1_object_word == dict2_object_word:
+                                                        is_object_word_found = True
+                                                        trues += 1
+                                                        break
+                                                if not is_object_word_found:
+                                                    falses += 1
+                            break
+                    if not competency_is_found:
+                        falses += 1
+
+        return [trues, falses]
 
     def __calculate_precision(self, true_positives, false_positives):
         return true_positives / (true_positives + false_positives) if true_positives or false_positives else 0.0
