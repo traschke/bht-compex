ebf35303a73411ecbf3fb36b7e49b7a51416bf25
Timo <timo.raschke@gmail.com>
2020-03-07 20:45:19 +0100

    Implement taxonomy search

--- compex/annotators.py
+++ compex/annotators.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 from stanfordnlp.server import CoreNLPClient
 
 from compex.competencies.competency_types import Competency, CompetencyObject, ObjectContext, Word, WordChunk
+from compex.taxonomy.taxonomy_manager import TaxonomyManager, BloomsTaxonomyLevelEnum
 
 # Die Studierenden beherrschen die grundlegenden Techniken zum wissenschaftlichen Arbeiten.
 # Die Studierenden können eine serverseitige Schnittstelle für moderne Webanwendungen konzipieren und implementieren.
@@ -28,12 +29,12 @@ class SemgrexAnnotator:
     pattern = '{tag:/VVINF|VVFIN|VVIZU/}=competency ?>dobj ({}=object ?>amod {tag:ADJA}=objectadja ?>det ({tag:NN}=objectdet ?>amod {tag:ADJA}=objectdetadja ?>det {tag:ART}=objectdetart)) ?>nmod ({}=context ?>/conj:.*/ {}=context2 ?>amod {tag:ADJA}=contextadja) ?>/conj:.*/ {tag:/VVINF|VVFIN|VVIZU/}=competency2'
     semgrex_properties = {"annotators": "tokenize,ssplit,depparse"}
 
-    def annotate(self, sentences: List[str], use_bloom: bool = False) -> Dict[str, List[Competency]]:
+    def annotate(self, sentences: List[str], taxonomy_verbs: Dict[str, BloomsTaxonomyLevelEnum] = None) -> Dict[str, List[Competency]]:
         """Annotates multiple sentences."""
         sentences = [sentence.strip() for sentence in sentences]
         text = " ".join(sentences)
         matches = self.__run_corenlp_server_semgrex(text)
-        competencies = self.__convert_to_competencies(matches)
+        competencies = self.__convert_to_competencies(matches, taxonomy_verbs)
         dicti = dict(zip(sentences, competencies))
         return dicti
 
@@ -42,51 +43,61 @@ class SemgrexAnnotator:
             matches = client.semgrex(text, self.pattern, properties=self.semgrex_properties)
         return matches
 
-    def __convert_to_competencies(self, input: Dict, use_bloom: bool = False) -> List[Competency]:
+    def __convert_to_competencies(self, input: Dict, taxonomy_verbs: Dict[str, BloomsTaxonomyLevelEnum] = None) -> List[Competency]:
         competencies: List[List[Competency]] = []
+
         for i, sentence in enumerate(input["sentences"]):
             sentence_competencies = []
             for key, match in sentence.items():
                 if key != "length":
                     temp = match
-                    competency = Competency(Word(temp["$competency"]["begin"], temp["$competency"]["text"]))
+                    competency_text = temp["$competency"]["text"]
+                    taxonomy_verb_found: bool = False
+                    if taxonomy_verbs:
+                        if competency_text in taxonomy_verbs:
+                            taxonomy_level: BloomsTaxonomyLevelEnum = taxonomy_verbs[competency_text]
+                            competency = Competency(Word(temp["$competency"]["begin"], competency_text), taxonomy_level=taxonomy_level)
+                            taxonomy_verb_found = True
+                    else:
+                        competency = Competency(Word(temp["$competency"]["begin"], competency_text))
 
-                    if "$object" in temp:
-                        object_chunk = WordChunk()
+                    if not taxonomy_verbs or taxonomy_verb_found:
+                        if "$object" in temp:
+                            object_chunk = WordChunk()
 
-                        if "$objectadja" in temp:
-                            object_chunk.words.append(Word(temp["$objectadja"]["begin"], temp["$objectadja"]["text"]))
+                            if "$objectadja" in temp:
+                                object_chunk.words.append(Word(temp["$objectadja"]["begin"], temp["$objectadja"]["text"]))
 
-                        object_chunk.words.append(Word(temp["$object"]["begin"], temp["$object"]["text"]))
+                            object_chunk.words.append(Word(temp["$object"]["begin"], temp["$object"]["text"]))
 
-                        if "$objectdet" in temp:
-                            if "$objectdetadja" in temp:
-                                object_chunk.words.append(Word(temp["$objectdetadja"]["begin"], temp["$objectdetadja"]["text"]))
-                            if "$objectdetart" in temp:
-                                object_chunk.words.append(Word(temp["$objectdetart"]["begin"], temp["$objectdetart"]["text"]))
+                            if "$objectdet" in temp:
+                                if "$objectdetadja" in temp:
+                                    object_chunk.words.append(Word(temp["$objectdetadja"]["begin"], temp["$objectdetadja"]["text"]))
+                                if "$objectdetart" in temp:
+                                    object_chunk.words.append(Word(temp["$objectdetart"]["begin"], temp["$objectdetart"]["text"]))
 
-                            object_chunk.words.append(Word(temp["$objectdet"]["begin"], temp["$objectdet"]["text"]))
-
-                        # Sort the words by index to remain context
-                        object_chunk.words.sort(key=lambda word: word.index)
-
-                        contexts = []
-
-                        if "$context" in temp:
-                            context_chunk = WordChunk()
-                            if "$contextadja" in temp:
-                                context_chunk.words.append(Word(temp["$contextadja"]["begin"], temp["$contextadja"]["text"]))
-
-                            context_chunk.words.append(Word(temp["$context"]["begin"], temp["$context"]["text"]))
+                                object_chunk.words.append(Word(temp["$objectdet"]["begin"], temp["$objectdet"]["text"]))
 
                             # Sort the words by index to remain context
-                            context_chunk.words.sort(key=lambda word: word.index)
+                            object_chunk.words.sort(key=lambda word: word.index)
 
-                            contexts.append(ObjectContext(context_chunk))
+                            contexts = []
+
+                            if "$context" in temp:
+                                context_chunk = WordChunk()
+                                if "$contextadja" in temp:
+                                    context_chunk.words.append(Word(temp["$contextadja"]["begin"], temp["$contextadja"]["text"]))
+
+                                context_chunk.words.append(Word(temp["$context"]["begin"], temp["$context"]["text"]))
+
+                                # Sort the words by index to remain context
+                                context_chunk.words.sort(key=lambda word: word.index)
+
+                                contexts.append(ObjectContext(context_chunk))
 
 
-                        # Add the object to the competency
-                        competency.objects.append(CompetencyObject(object_chunk, contexts))
-                    sentence_competencies.append(competency)
+                            # Add the object to the competency
+                            competency.objects.append(CompetencyObject(object_chunk, contexts))
+                        sentence_competencies.append(competency)
             competencies.append(sentence_competencies)
         return competencies
--- tests/test_annotators.py
+++ tests/test_annotators.py
@@ -3,6 +3,7 @@ import os
 
 from compex.annotators import SemgrexAnnotator
 from compex.competencies.competency_types import Competency, CompetencyObject, ObjectContext, Word, WordChunk
+from compex.taxonomy.taxonomy_manager import TaxonomyManager, BloomsTaxonomyLevelEnum
 
 class TestSemgrexAnnotator:
     def test_simple(self):
@@ -32,3 +33,69 @@ class TestSemgrexAnnotator:
 
         assert result[sample[0]][0] == c1
         assert result[sample[1]][0] == c2
+
+    def test_simple_with_taxonomy(self):
+        os.environ["CORENLP_HOME"] = "/home/timo/Downloads/stanford-corenlp-full-2018-10-05"
+        sample = [
+            "Studierende implementieren.",
+            "Die Studierenden kennen die Grundlagen des wissenschaftlichen Arbeitens."
+            ]
+
+        c1 = Competency(Word(1, "implementieren"))
+        c2 = Competency(
+            Word(2, "kennen"),
+            CompetencyObject(
+                WordChunk(
+                    [
+                        Word(4, "Grundlagen"),
+                        Word(5, "des"),
+                        Word(6, "wissenschaftlichen"),
+                        Word(7, "Arbeitens"),
+                    ]
+                )
+            )
+        )
+
+        taxonomy_manager = TaxonomyManager()
+        test_dir = os.path.dirname(__file__)
+        with open(os.path.join(test_dir, "test.json"), 'r') as json_file:
+            taxonomy_verbs = taxonomy_manager.read_json(json_file)
+
+            annotator = SemgrexAnnotator()
+            result = annotator.annotate(sample, taxonomy_verbs)
+
+            assert result[sample[0]][0] == c1
+            assert result[sample[1]][0] == c2
+
+    def test_simple_with_taxonomy_not_found(self):
+            os.environ["CORENLP_HOME"] = "/home/timo/Downloads/stanford-corenlp-full-2018-10-05"
+            sample = [
+                "Studierende kochen.",
+                "Die Studierenden kennen die Grundlagen des wissenschaftlichen Arbeitens."
+                ]
+
+            # c1 = Competency(Word(1, "implementieren"))
+            c2 = Competency(
+                Word(2, "kennen"),
+                CompetencyObject(
+                    WordChunk(
+                        [
+                            Word(4, "Grundlagen"),
+                            Word(5, "des"),
+                            Word(6, "wissenschaftlichen"),
+                            Word(7, "Arbeitens"),
+                        ]
+                    )
+                )
+            )
+
+            taxonomy_manager = TaxonomyManager()
+            test_dir = os.path.dirname(__file__)
+            with open(os.path.join(test_dir, "test.json"), 'r') as json_file:
+                taxonomy_verbs = taxonomy_manager.read_json(json_file)
+
+                annotator = SemgrexAnnotator()
+                result = annotator.annotate(sample, taxonomy_verbs)
+
+                assert not result[sample[0]]
+                assert result[sample[1]][0] == c2
